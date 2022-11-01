cb6ba1c2e60a6affb43ad13a6b5c6de713f045c3
Andres 'SHK' F. P <andrs.fp@gmail.com>
2022-11-01 19:19:47 +0100

    adds slider for both desktop and mobile

--- script/main.js
+++ script/main.js
@@ -1,6 +1,32 @@
-function loaded() {
-    document.querySelector(".header-hamburger").addEventListener("click", () => {
-        document.querySelector(".header__nav").classList.toggle("header__nav--closed");
-        document.querySelector(".header-hamburger").classList.toggle("header-hamburger--open");
-    });
-};
+// // core version + navigation, pagination modules:
+// import Swiper, { Navigation, Pagination } from 'swiper';
+// // import Swiper and modules styles
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js';
+
+document.querySelector(".header-hamburger").addEventListener("click", () => {
+    document.querySelector(".header__nav").classList.toggle("header__nav--closed");
+    document.querySelector(".header-hamburger").classList.toggle("header-hamburger--open");
+});
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },  
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 5000,
+    },
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 40,
+});
