3d59bfe043d3e17619da013a897d326f907e71b8
Andres 'SHK' F. P <andrs.fp@gmail.com>
2023-01-18 07:40:27 +0100

    attempts to alliviate file tail accumulation concerns

--- scripts/gitUnprep.py
+++ scripts/gitUnprep.py
@@ -4,38 +4,44 @@ from itertools import chain
 scssDir = Path('./style')
 scssFiles = scssDir.glob('*.scss')
 for file in scssFiles:
-    with file.open('r+', encoding="utf-8") as styleFile:
+    content = ""
+    with file.open('r', encoding="utf-8") as styleFile:
+        styleFile.reconfigure(write_through=True)
         content = styleFile.read()
-        content = content.replace(
+    content = content.replace(
             '---\n---\n\n$baseurl:"{{ site.baseurl }}";\n',
             '')
-        content = content.replace('@ import \'',
+    content = content.replace('@ import \'',
             '@ import \'../_sass/_')
-        styleFile.seek(0, 0)
+    with file.open('w', encoding="utf-8") as styleFile:
         styleFile.write(content)
-        print(str(file) + ' updated')
+    print(str(file) + ' updated')
 
 scssPartialDir = Path('./_sass')
 scssPartialFiles = scssPartialDir.glob('*.scss')
 for file in scssPartialFiles:
-    with file.open('r+', encoding="utf-8") as partialFile:
+    content = ""
+    with file.open('r+', encoding="utf-8", buffering=2) as partialFile:
+        partialFile.reconfigure(write_through=True)
         content = partialFile.read()
-        content = content.replace('$baseurl + "/img',
+    content = content.replace('$baseurl + "/img',
             '"../img')
-        partialFile.seek(0, 0)
+    with file.open('w', encoding="utf-8") as partialFile:
         partialFile.write(content)
-        print(str(file) + ' updated')
+    print(str(file) + ' updated')
 
 baseDir = Path('.')
 htmlFiles = baseDir.rglob('*.html')
 phpFiles = baseDir.rglob('*.php')
 swapFiles = chain(htmlFiles, phpFiles)
 for file in swapFiles:
-    with file.open('r+', encoding="utf-8") as swapFile:
+    content = ""
+    with file.open('r+', encoding="utf-8", buffering=2) as swapFile:
+        swapFile.reconfigure(write_through=True)
         content = swapFile.read()
-        content = content.replace('/style/',
+    content = content.replace('/style/',
             '/css/')
-        swapFile.seek(0, 0)
+    with file.open('w', encoding="utf-8") as swapFile:
         swapFile.write(content)
-        print(str(file) + ' updated')
+    print(str(file) + ' updated')
 # TODO fix end of file duplication bug on unprepping
